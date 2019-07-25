ee1563b7f56f385d6b0e5c3a43152c6f9badfb33
Michael Stickel <Michael.Stickel@baslerweb.com>
2019-07-25 21:15:07 +0200

    Now use two dmx-cards to pass frames back and forth.

--- src/drivers/dmx512/boards/dmx512_dummy.c
+++ src/drivers/dmx512/boards/dmx512_dummy.c
@@ -15,40 +15,40 @@ static unsigned int ports_count = 8;
 module_param(ports_count, uint, 0);
 MODULE_PARM_DESC(ports_count, "number of ports to create for both dummy devices");
 
+static struct dmx512_device *g_dmx_devices[2] = {0,0};
+
 static int dmx512_dummy_send_frame (struct dmx512_port * port, struct dmx512_framequeue_entry * frame)
 {
+	const int other_device_index = (port->device == g_dmx_devices[0]) ? 1 : 0;
+	struct dmx512_device *other_device = g_dmx_devices[other_device_index];
+
 	/*
 	 * Send the frame to the same port of the receive queue of the other device.
 	 * No need to change the frame, as the data is valid on the recive path.
 	 * The timestamp difference is not worth it.
 	 */
-	struct dmx512_port * dst_port = dmx512_port_by_index(port->device, dmx512_port_index(port) ^ 1);
+	struct dmx512_port * dst_port = dmx512_port_by_index(other_device, dmx512_port_index(port));
 	if (dst_port)
 	    dmx512_received_frame(dst_port, frame);
 	return 0;
 }
 
-
-static struct dmx512_device *dmx = 0;
-static int __init dmx512_dummy_driver_init(void)
+static struct dmx512_device * create_dummy_dmx_device(const char * name)
 {
         int ret;
 	int i;
+	struct dmx512_device *dmx = kzalloc(sizeof(*dmx), GFP_KERNEL);
+	if (dmx == NULL)
+		return ERR_PTR(-ENOMEM);
 
-	printk(KERN_INFO "loading dmx512 dummy driver\n");
-
-        dmx = kzalloc(sizeof(*dmx), GFP_KERNEL);
-        if (dmx == NULL)
-                return -ENOMEM;
-
-	dmx->name = "dummy";
+	dmx->name = kstrdup(name ?: "unknown", GFP_KERNEL);
 	dmx->parent = 0;
 	dmx->owner = THIS_MODULE;
 	ret = register_dmx512_device(dmx, 0);
 	if (ret < 0) {
 		kfree(dmx);
-                printk(KERN_ERR "could not register dmx512 dummy device");
-                return ret;
+		printk(KERN_ERR "could not register dmx512 dummy device");
+		return ERR_PTR(ret);
 	}
 
 	for (i = 0; i < ports_count; ++i)
@@ -63,16 +63,38 @@ static int __init dmx512_dummy_driver_init(void)
 			dmx512_add_port(dmx, port);
 		}
 	}
-        return ret;
+	return dmx;
+}
+
+static int __init dmx512_dummy_driver_init(void)
+{
+	struct dmx512_device *dmx;
+
+	printk(KERN_INFO "loading dmx512 dummy driver\n");
+	dmx = create_dummy_dmx_device("card0");
+	if (IS_ERR(dmx))
+		return PTR_ERR(dmx);
+	g_dmx_devices[0] = dmx;
+
+	dmx = create_dummy_dmx_device("card1");
+	if (IS_ERR(dmx))
+		return PTR_ERR(dmx);
+	g_dmx_devices[1] = dmx;
+
+        return 0;
 }
 
 static void __exit dmx512_dummy_driver_exit(void)
 {
-    if (!dmx)
-	return;
-    unregister_dmx512_device(dmx);
-    kfree(dmx);
-    printk(KERN_INFO "unloading dmx512 dummy driver\n");
+	if (g_dmx_devices[0])
+		unregister_dmx512_device(g_dmx_devices[0]);
+	if (g_dmx_devices[1])
+		unregister_dmx512_device(g_dmx_devices[1]);
+	if (g_dmx_devices[0])
+		kfree(g_dmx_devices[0]);
+	if (g_dmx_devices[1])
+		kfree(g_dmx_devices[1]);
+	printk(KERN_INFO "unloading dmx512 dummy driver\n");
 }
 
 module_init(dmx512_dummy_driver_init);
