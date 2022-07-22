1be65ee21a6d0b72e54c200ce7a07c2e0c33b27d
Michael Stickel <michael@cubic.org>
2022-07-22 23:03:07 +0200

    Implemented correct uio code.

--- src/simulation/uart/wbuart_16x50_uio.c
+++ src/simulation/uart/wbuart_16x50_uio.c
@@ -1,114 +1,119 @@
-
 #include "wbuart_16x50.h"
+#include "uio.h"
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 
 #include <sys/ioctl.h>
-#include <linux/parport.h>
-#include <linux/ppdev.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include <pthread.h>
 
-struct pc16x50_epp
+
+struct pc16x50_uio
 {
-  struct pc16x50 uart;
-  int fd;
+    struct pc16x50 uart;
+    struct uio_handle * uio;
+
+    int run;
+    pthread_t  irq_thread;
+
+    void (*irqfunc)(int, void *);
+    void * irqarg;
 };
-
-
-int EppWriteAddr (int parport, unsigned char *buff, size_t size)
-{
-  int mode = IEEE1284_MODE_EPP | IEEE1284_ADDR;
-  if (ioctl(parport, PPSETMODE, &mode))
-    return -1;
-  return write (parport, buff, size);
-}
-
-int EppReadAddr (int parport, unsigned char *buff, size_t size)
-{
-  int mode = IEEE1284_MODE_EPP | IEEE1284_ADDR;
-  if (ioctl(parport, PPSETMODE, &mode))
-    return -1;
-  return read (parport, buff, size);
-}
-
-int EppWriteData (int parport, unsigned char *buff, size_t size)
-{
-  int mode = IEEE1284_MODE_EPP | IEEE1284_DATA;
-  if (ioctl(parport, PPSETMODE, &mode))
-    return -1;
-  return write (parport, buff, size);
-}
-
-int EppReadData (int parport, unsigned char *buff, size_t size)
-{
-  int mode = IEEE1284_MODE_EPP | IEEE1284_DATA;
-  if (ioctl(parport, PPSETMODE, &mode))
-    return -1;
-  return read (parport, buff, size);
-}
 
 
 
 static void wbuart_reg_write(void * ctx, int reg, u8 value)
 {
-  struct pc16x50_epp * u = (struct pc16x50_epp *)ctx;
-  unsigned char adr = reg;
-  if (EppWriteAddr (u->fd, &adr, 1) < 0)
-    return;
-  EppWriteData (u->fd, &value, 1);
+    printf ("wbuart_reg_write(reg:%02X, value:%02X\n", reg, value);
+    struct pc16x50_uio * u = (struct pc16x50_uio *)ctx;
+    uio_poke8(u->uio, reg, value);
 }
 
 static u8 wbuart_reg_read(void * ctx, int reg)
 {
-  struct pc16x50_epp * u = (struct pc16x50_epp *)ctx;
-  unsigned char adr = reg;
-  if (EppWriteAddr (u->fd, &adr, 1) < 0)
-    return 0;
-  u8 value;
-  if (EppReadData (u->fd, &value, 1) < 0)
-    return 0;
-  return value;
+    printf ("wbuart_reg_read(reg:%02X)\n", reg);
+    struct pc16x50_uio * u = (struct pc16x50_uio *)ctx;
+    return uio_peek8 (u->uio, reg);
 }
+
+
+static void * interrupt_thread_function(void * arg)
+{
+    struct pc16x50_uio * u = (struct pc16x50_uio *)arg;
+
+    while (1)
+    {
+	struct timeval timeout;
+        timeout.tv_sec = 1;
+        timeout.tv_usec = 0;
+
+	if (!u->run)
+	    return 0;
+
+	uio_enable_interrupt(u->uio);
+	if (uio_wait_for_interrupt_timeout(u->uio, &timeout) >= 0)
+	{
+	    if  (u->irqfunc)
+		u->irqfunc(0, u->irqarg);
+	}
+    }
+}
+
+
 
 struct pc16x50 * wbuart_pc16x50_create()
 {
-  const int pfd  = open("/dev/parport0", O_RDWR);
-  if ((ioctl(pfd, PPEXCL) < 0) || (ioctl(pfd, PPCLAIM) < 0))
+    struct uio_handle * uio = 0;
+    const char * uio_name = getenv("UIO_NAME");
+    if (!uio_name)
+	return 0;
+  
+    uio = uio_open (uio_name, 4096);
+    if (!uio)
     {
-      close(pfd);
-      return 0;
+	printf ("failed to claim parport device\n");
+	return 0;
     }
 
-  struct pc16x50_epp * u = malloc(sizeof(struct pc16x50_epp));
-  pc16x50_init(&u->uart, wbuart_reg_write, wbuart_reg_read, u);
-  u->fd = pfd;
+    struct pc16x50_uio * u = malloc(sizeof(struct pc16x50_uio));
+    pc16x50_init(&u->uart, wbuart_reg_write, wbuart_reg_read, u);
+    u->uio = uio;
 
-  // open UIO device and start pthread as irq handler.
+    u->run = 1;
+    pthread_create(&u->irq_thread, NULL, interrupt_thread_function, u);
 
-  return &u->uart;
+    return &u->uart;
 }
 
 void wbuart_set_irqhandler(struct pc16x50 * u,
                            void (*irqfunc)(int, void *),
                            void * irqarg)
 {
-  (void)u;
+    struct pc16x50_uio * uart = (struct pc16x50_uio *)u;
+    uart->irqfunc = irqfunc;
+    uart->irqarg = irqarg;
 }
 
 void wbuart_delete(struct pc16x50 * u)
 {
   if (u)
     {
-      struct pc16x50_epp * uart = (struct pc16x50_epp *)u;
-      ioctl(uart->fd, PPRELEASE);
-      close(uart->fd);
+      struct pc16x50_uio * uart = (struct pc16x50_uio *)u;
+
+      uio_disable_interrupt(uart->uio);
+
+      uart->run = 0;
+      pthread_join(uart->irq_thread, 0);
+
+      uio_close(uart->uio);
+
       free(u);
     }
 }
-
